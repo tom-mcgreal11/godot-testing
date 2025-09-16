6891fe749b122680efe4982843eafe721d8e500e
tom-mcgreal11 <tom.mcgreal11@gmail.com>
2025-09-16 14:53:32 +0100

    Added rotations, 5 degrees of freedom

--- assets/ship_movement.gd
+++ assets/ship_movement.gd
@@ -3,18 +3,29 @@ extends CharacterBody3D
 @onready var speed := 20
 @onready var gravity := 9.8
 @onready var jump_strength := 10
+@onready var yaw_rate := 1
+@onready var rotate_rate := 1
+@onready var look_rate := 1
 
 func _physics_process(delta: float) -> void:
 	var direction = Vector3.ZERO
-	
-	direction.z = Input.get_axis("move_forward", "move_backward") * speed
-	direction.x = Input.get_axis("move_left", "move_right") * speed
-
+	var yaw_input := 0
+	var rotate_input := 0
+	var look_input := 0
+	direction = -(-transform.basis.z * Input.get_axis("move_forward", "move_backward") \
+	-transform.basis.x * Input.get_axis("move_left", "move_right"))
+	#rotate(Vector3.RIGHT, 1.0 * delta)
+	yaw_input = - Input.get_axis("yaw_left", "yaw_right") * yaw_rate
+	rotate(Vector3.UP, yaw_input * delta)
+	rotate_input = Input.get_axis("rotate_anticlockwise", "rotate_clockwise") * rotate_rate
+	rotate(Vector3.FORWARD, rotate_input * delta)
+	look_input = Input.get_axis("look_up", "look_down") * look_rate
+	rotate(Vector3.RIGHT, look_input * delta )
 	
 	if not is_on_floor():
 		direction.y -= gravity * delta
 	else:
 		if Input.is_action_pressed("jump"):
 			direction.y += jump_strength	
-	velocity = direction
+	velocity = direction * speed
 	move_and_slide()
--- camera_spring.gd
+++ camera_spring.gd
@@ -6,11 +6,11 @@ func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion && Input.mouse_mode == 2:
-		rotation.y -= event.relative.x * mouse_sensitivity
-		rotation.y = wrapf(rotation.y, 0 , TAU) 
-		rotation.x -= event.relative.y * mouse_sensitivity
-		rotation.x = clamp(rotation.x, -PI/2, PI/4)
+	#if event is InputEventMouseMotion && Input.mouse_mode == 2:
+	#	rotation.y -= event.relative.x * mouse_sensitivity
+	#	rotation.y = wrapf(rotation.y, 0 , TAU) 
+	#	rotation.x -= event.relative.y * mouse_sensitivity
+	#	rotation.x = clamp(rotation.x, -PI/2, PI/4)
 
 	if event.is_action_pressed("wheel_up"):
 		spring_length -= 0.1
